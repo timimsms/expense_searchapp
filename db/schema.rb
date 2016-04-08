13693f2881fb250c099933c37cf93644882dcc59
Tim Walsh <tim@useed.org>
2016-04-08 08:55:44 -0700

    Adds Card model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408150819) do
+ActiveRecord::Schema.define(version: 20160408153647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "last_four_digits"
+    t.string   "owner"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "cards", ["last_four_digits"], name: "index_cards_on_last_four_digits", unique: true, using: :btree
 
   create_table "pg_search_documents", force: :cascade do |t|
     t.text     "content"
