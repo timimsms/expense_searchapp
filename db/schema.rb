f2213bab70102e1fb8e8b187ee826227fae1a3c2
Tim Walsh <tim@useed.org>
2016-04-08 07:22:41 -0700

    Updates Transction
    Updates the `is_complete` column of the Transaction model to default to `false`.
    
    Note: This field is intended to be part of a workflow to keep track of which Transactions have been properly categorized.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160408133702) do
+ActiveRecord::Schema.define(version: 20160408141049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20160408133702) do
     t.string   "reported_date"
     t.string   "reported_amount"
     t.string   "reported_description"
-    t.boolean  "is_complete"
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.boolean  "is_complete",          default: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
   end
 
 end
