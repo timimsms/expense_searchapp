44b96c91f9d95ed33fb4769ab51ae94e97693980
Tim Walsh <tim@useed.org>
2016-04-10 00:36:51 -0700

    Notes on Category future work.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,11 @@
+# TODO - Add PgSeach; search method should be multi-search over
+#        transaction_keyword and label
+# TODO - Label should be used as an override when a label needs
+#        to be manually applied over the raw transaction_keyword
+#        found in the data
+# TODO - The Category transaction_keyword attribute should be migrated to a
+#        JSON hash or separate tableto allow support for multiple keywords.
+#        May also want to link to support BankTransaction <-> Keywords.
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :bank_transactions,
                           join_table: :bank_transactions_categories
